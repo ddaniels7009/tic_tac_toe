2245c380554aab5b9a1a229f3672fe4feb81ced6
david daniels <ddaniels7009@gmail.com>
2021-06-27 15:23:10 -0600

    Added multiple new features.

--- app.js
+++ app.js
@@ -7,39 +7,74 @@ body = document.body;
 // STORE THE GAMEBOARD INSIDE AN ARRAY INSIDE OF A GAMEBOARD OBJECT --- USE A MODULE SINCE YOU ONLY HAVE ONE OF THEM
 const board = (function () {
 
-
     let rows = document.getElementsByClassName('rows');
     let squares = document.getElementsByClassName('square');
     let textContainer = document.getElementsByClassName('textContainer');
+    let resultsPanel = document.getElementsByClassName('results');
 
-    const gameBoard = [
+    let gameBoard = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8]
     ];
 
-    // Create 3 rows
-    for (let i = 0; i < 3; i++) {
-        let row = document.createElement('div');
-        body.appendChild(row);
-        row.classList.add('rows')
-        row.setAttribute('id', i + 1);
+
+    const createBoard = function () {
+
+
+        // Create 3 rows
+        for (let i = 0; i < 3; i++) {
+            let row = document.createElement('div');
+            body.appendChild(row);
+            row.classList.add('rows')
+            row.setAttribute('id', i + 1);
+        }
+
+        // Create 9 squares and append them to the rows
+        for (let i = 0; i < rows.length; i++) {
+            for (let j = 0; j < 3; j++) {
+                square = document.createElement('div');
+                square.classList.add('square');
+                square.setAttribute('id', gameBoard[i][j]);
+
+                let textContainer = document.createElement('p');
+                textContainer.classList.add('textContainer');
+                square.appendChild(textContainer);
+
+                rows[j].appendChild(square);
+                gameBoard[i][j] = square;
+            }
+        }
+
+        // Create results panel DOM elements
+        const resultsPanel = document.createElement('div');
+        resultsPanel.classList.add('results');
+        body.appendChild(resultsPanel);
+
+        const resultsTextBox = document.createElement('div');
+        resultsTextBox.setAttribute('id', 'resultsTextBox');
+        resultsTextBox.innerText = "jj";
+        resultsPanel.appendChild(resultsTextBox);
+
+        // Create reset button DOM elements
+        let resetButton = document.createElement('button');
+        resetButton.innerText = "New Game"
+        resultsPanel.appendChild(resetButton);
+        resetButton.addEventListener('click', function () { deleteBoard(); createBoard(); placeMarker("X");})
+
     }
 
-    // Create 9 squares and append them to the rows
-    for (let i = 0; i < rows.length; i++) {
-        for (let j = 0; j < 3; j++) {
-            square = document.createElement('div');
-            square.classList.add('square');
-            square.setAttribute('id', gameBoard[i][j]);
+    //Create initial board
+    createBoard();
 
-            let textContainer = document.createElement('p');
-            textContainer.classList.add('textContainer');
-            square.appendChild(textContainer);
+    // Delete the board at the end of the game.
+    const deleteBoard = function () {
 
-            rows[j].appendChild(square);
-            gameBoard[i][j] = square;
+        for (let i = 1; i < 4; i++) {
+            body.removeChild(document.getElementById(i));
         }
+        body.removeChild(resultsPanel[0]);
+
     }
 
     // Must call this function for it to execute
@@ -48,34 +83,36 @@ const board = (function () {
         for (let i = 0; i < 9; i++) {
 
             if (e == "X") {
-                if (textContainer[i].innerText == "X" || textContainer[i].innerText == "O") {
+
+                if ((textContainer[i].innerText == "X" || textContainer[i].innerText == "O")) {
 
                 }
                 else {
-                    textContainer[i].addEventListener('click', function () { textContainer[i].innerText = e; placeMarker("O") });
+                    textContainer[i].addEventListener('click', function () { textContainer[i].innerText = e; placeMarker("O"); game.checkForWin() });
                 }
             }
             else if (e == "O") {
-                if (textContainer[i].innerText == "X" || textContainer[i].innerText == "O") {
+
+                if ((textContainer[i].innerText == "X" || textContainer[i].innerText == "O")) {
 
                 }
+
                 else {
-                    textContainer[i].addEventListener('click', function () { textContainer[i].innerText = e; placeMarker("X") });
+                    textContainer[i].addEventListener('click', function () { textContainer[i].innerText = e; placeMarker("X"); game.checkForWin() });
                 }
             }
         }
-
     }
 
     // Call initial placemarker
-    placeMarker("X")
+    placeMarker("X");
 
-    return { gameBoard, placeMarker, textContainer };
+    return { gameBoard, placeMarker, textContainer, createBoard, resultsPanel, resultsTextBox };
 
 
 })();
 
-
+//board.resultsTextBox.innerText = "ggg"////////////////////////////////////
 
 
 // CREATE PLAYER OBJECT USING A FACTORY --- SINCE YOU HAVE MORE THAN ONE OF THEM
@@ -95,19 +132,108 @@ const player2 = player('Alison');
 
 
 
-// CREATE AN OBJECT TO CONTROL THE GLOW OF THE GAME --- USE A MODULE SINCE YOU ONLY HAVE ONE OF THEM
+// CREATE AN OBJECT TO CONTROL THE FLOW OF THE GAME --- USE A MODULE SINCE YOU ONLY HAVE ONE OF THEM
 const game = (function () {
 
 
-    const checkForWin = function() {
+    const checkForWin = function () {
+
+        //*** Check X's ***
+
+        // Checks for horizontal win
         if (board.textContainer[0].innerText == "X" && board.textContainer[1].innerText == "X" && board.textContainer[2].innerText == "X") {
-            console.log("game over");
+            board.resultsTextBox.innerText = "Game over. X wins";
+            return "game over"
+
         }
+        else if (board.textContainer[3].innerText == "X" && board.textContainer[4].innerText == "X" && board.textContainer[5].innerText == "X") {
+            board.resultsTextBox.innerText = "Game over. X wins";
+            return "game over";
+        }
+        else if (board.textContainer[6].innerText == "X" && board.textContainer[7].innerText == "X" && board.textContainer[8].innerText == "X") {
+            board.resultsTextBox.innerText = "Game over. X wins";
+            return "game over";
+        }
+        // Check for diagonal win
+        else if (board.textContainer[0].innerText == "X" && board.textContainer[4].innerText == "X" && board.textContainer[8].innerText == "X") {
+            board.resultsTextBox.innerText = "Game over. X wins";
+            return "game over";
+        }
+        else if (board.textContainer[2].innerText == "X" && board.textContainer[4].innerText == "X" && board.textContainer[6].innerText == "X") {
+            board.resultsTextBox.innerText = "Game over. X wins";
+            return "game over";
+        }
+        // Check for vertical win
+        else if (board.textContainer[0].innerText == "X" && board.textContainer[3].innerText == "X" && board.textContainer[6].innerText == "X") {
+            board.resultsTextBox.innerText = "Game over. X wins";
+            return "game over";
+
+        }
+        else if (board.textContainer[1].innerText == "X" && board.textContainer[4].innerText == "X" && board.textContainer[7].innerText == "X") {
+            board.resultsTextBox.innerText = "Game over. X wins";
+            return "game over";
+        }
+        else if (board.textContainer[2].innerText == "X" && board.textContainer[5].innerText == "X" && board.textContainer[8].innerText == "X") {
+            board.resultsTextBox.innerText = "Game over. X wins";
+            return "game over";
+
+        }
+
+        //*** Check O's ***
+
+        // Checks for horizontal win
+        else if (board.textContainer[0].innerText == "O" && board.textContainer[1].innerText == "O" && board.textContainer[2].innerText == "O") {
+            board.resultsTextBox.innerText = "Game over. O wins";
+            return "game over";
+
+        }
+        else if (board.textContainer[3].innerText == "O" && board.textContainer[4].innerText == "O" && board.textContainer[5].innerText == "O") {
+            board.resultsTextBox.innerText = "Game over. O wins";
+            return "game over";
+
+        }
+        else if (board.textContainer[6].innerText == "O" && board.textContainer[7].innerText == "O" && board.textContainer[8].innerText == "O") {
+            board.resultsTextBox.innerText = "Game over. O wins";
+            return "game over";
+
+        }
+        // Check for diagonal win
+        else if (board.textContainer[0].innerText == "O" && board.textContainer[4].innerText == "O" && board.textContainer[8].innerText == "O") {
+            board.resultsTextBox.innerText = "Game over. O wins";
+            return "game over";
+
+        }
+        else if (board.textContainer[2].innerText == "O" && board.textContainer[4].innerText == "O" && board.textContainer[6].innerText == "O") {
+            board.resultsTextBox.innerText = "Game over. O wins";
+            return "game over";
+
+        }
+        // Check for vertical win
+        else if (board.textContainer[0].innerText == "O" && board.textContainer[3].innerText == "O" && board.textContainer[6].innerText == "O") {
+            board.resultsTextBox.innerText = "Game over. O wins";
+            return "game over";
+
+        }
+        else if (board.textContainer[1].innerText == "O" && board.textContainer[4].innerText == "O" && board.textContainer[7].innerText == "O") {
+            board.resultsTextBox.innerText = "Game over. O wins";
+            return "game over";
+
+        }
+        else if (board.textContainer[2].innerText == "O" && board.textContainer[5].innerText == "O" && board.textContainer[8].innerText == "O") {
+            board.resultsTextBox.innerText = "Game over. O wins";
+            return "game over";
+
+        }
+        else {
+            return "";
+        }
+
+
     }
 
 
 
-    return {};
+    return { checkForWin };
 
 })();
 
